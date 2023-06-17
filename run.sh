6e42942c9f683658b6f1784809875c6a7c4c6917
tiankaima <tiankaima@users.noreply.github.com>
2023-06-17 09:16:32 +0000

    deploy: 9f1dd986da90f1bd968790486b62045932bdc485

---
+++ run.sh
@@ -0,0 +1,27 @@
+# assuming Ubuntu 20.04, skipping updates
+
+# sudo apt-get update
+# sudo apt-get upgrade -y
+sudo apt-get install texlive-full imagemagick python3
+mkdir img
+ls -al
+
+# .tex files are stored under src/
+# each file is a standalone tikzpicture, compile them to png, store in img/ with the same name
+# the png files are used in the README.md
+
+# compile all .tex files in src/
+for f in src/*.tex; do
+    # get filename without extension
+    filename=$(basename -- "$f")
+    filename="${filename%.*}"
+    # compile to pdf
+    pdflatex -output-directory img/ $f
+    # convert pdf to png
+    convert -density 300 img/$filename.pdf -quality 90 img/$filename.png
+    # remove pdf
+    rm img/$filename.pdf
+done
+
+# run python script to generate README.md
+python3 generate_readme.py
